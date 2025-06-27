e189d47ed8120c9e46913c8b25ad020cb9b52c7a
amorgula <game14-00@mail.ru>
2025-06-27 23:29:48 +0700

    updating UI for file or text choosing

--- encryptorinator.cpp
+++ encryptorinator.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 #include <vector>
+using namespace std;
+bool file_or_text (bool file = 0) {
+    cout << "Choose options and write number:\n1.File\n2.Text\n";
+    char choose;
+    cin >> choose;
+    switch (choose)
+    {
+    case '1':
+        return (1);
+    case '2':
+        return (0);
+    default:
+        cout << "invalid input";
+        return (file_or_text(file = 0));
+    }
+}
 int main () {
+    bool file = 0;
+    file_or_text(file);
     return (0);
 }
